f0afd66bd8723f811622989760526e94b9c678a6
Chetanpatil03 <chetanpatil6303@gmail.com>
2026-02-11 17:35:56 +0530

    Initial commit with basic UI and REST API usage

---
+++ src/main/java/com/example/service/WeatherService.java
@@ -0,0 +1,21 @@
+package com.example.service;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class WeatherService {
+    @Value("${weather.api.key}")
+    private String apiKey;
+
+    @Value("${weather.api.url}")
+    private String apiUrl;
+
+    private final RestTemplate restTemplate = new RestTemplate();
+
+    public String getWeatherDetails(String city){
+        String url = apiUrl + "?q=" + city + "&appid="+ apiKey + "&units=metric";
+        return restTemplate.getForObject(url, String.class);
+    }
+}
